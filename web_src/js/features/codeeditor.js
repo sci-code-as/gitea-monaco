be349959efefe204d88e5467590da22d1602f044
Ivan Malkov <ozonep@yandex.ru>
2020-05-18 12:55:47 +0300

    For now removed markdown support since it requires too many extra grammars

--- web_src/js/features/codeeditor.js
+++ web_src/js/features/codeeditor.js
@@ -17,12 +17,9 @@ const grammarConfigurations = [
   { language: 'css', extension: '.css', scopeName: 'source.css', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/css.tmLanguage.json' },
   { language: 'go', extension: '.go', scopeName: 'source.go', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/go.tmLanguage.json' },
   { language: 'html', extension: '.html', scopeName: 'text.html.basic', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/html.tmLanguage.json' },
-  { language: 'htmlderivative', extension: '.ht', scopeName: 'text.html.derivative', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/html-derivative.tmLanguage.json' },
   { language: 'javascript', extension: '.js', scopeName: 'source.js', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/JavaScript.tmLanguage.json' },
-  { language: 'javascriptreact', extension: '.jsx', scopeName: 'source.js.jsx', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/JavaScriptReact.tmLanguage.json' },
   { language: 'json', extension: '.json', scopeName: 'source.json', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/JSON.tmLanguage.json' },
   { language: 'python', extension: '.py', scopeName: 'source.python', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/MagicPython.tmLanguage.json' },
-  { language: 'markdown', extension: '.md', scopeName: 'text.html.markdown', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/markdown.tmLanguage.json' },
   { language: 'typescript', extension: '.ts', scopeName: 'source.ts', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/TypeScript.tmLanguage.json' },
   { language: 'yaml', extension: '.yaml', scopeName: 'source.yaml', url: 'https://raw.githubusercontent.com/ozonep/gitea-monaco/tokenizers/web_src/js/features/grammars/yaml.tmLanguage.json' },
 ];
