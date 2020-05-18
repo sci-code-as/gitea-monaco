dd1c54f1265ae9e2b2ea04047113040abcd69362
Ivan Malkov <ozonep@yandex.ru>
2020-05-18 11:07:56 +0300

    removed comment

--- web_src/js/features/monacowasm.js
+++ web_src/js/features/monacowasm.js
@@ -71,7 +71,7 @@ async function createRegistry(scopeNameToTextMateGrammarURL) {
 }
 
 async function loadVSCodeOnigurumWASM() {
-  const response = await fetch('https://storage.cloud.google.com/giteaassets/onig.wasm?');
+  const response = await fetch('https://storage.cloud.google.com/giteaassets/onig.wasm');
   const contentType = response.headers.get('content-type');
   if (contentType === 'application/wasm') {
     return response;
