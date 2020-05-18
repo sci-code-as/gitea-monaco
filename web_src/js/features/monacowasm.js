f50a2d00f5fa42bef1d7d46f622d3ac38d7cc5a1
Ivan Malkov <ozonep@yandex.ru>
2020-05-18 09:31:37 +0300

    Now fetching onig.wasm for CDN

--- web_src/js/features/monacowasm.js
+++ web_src/js/features/monacowasm.js
@@ -71,7 +71,7 @@ async function createRegistry(scopeNameToTextMateGrammarURL) {
 }
 
 async function loadVSCodeOnigurumWASM() {
-  const response = await fetch('./onig.wasm');
+  const response = await fetch('https://cdn.jsdelivr.net/npm/vscode-oniguruma@1.3.1/release/onig.wasm');
   const contentType = response.headers.get('content-type');
   if (contentType === 'application/wasm') {
     return response;
